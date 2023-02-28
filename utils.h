9255b18bf9b5ca4425f578c4c4a88d9687aac25b
mnsir <mnsir@ya.ru>
2023-02-28 22:10:06 +0300

    DivideSegment

--- utils.h
+++ utils.h
@@ -11,3 +11,32 @@ inline auto va(std::string_view sv)
 	*out = '\0';
 	return arr.data();
 }
+
+
+
+template <std::integral T, typename... Args>
+constexpr auto DivideSegment(T total_length, T margin, T interval, Args... parts)
+{
+	constexpr auto count = sizeof...(parts) + 1;
+	static_assert(count > 1, "Invalid total parts sizes count");
+
+	std::array<T, count * 2> v;
+	size_t i = 0;
+	size_t num = 0;
+	T now = 0;
+	auto iteration = [&](T part)
+	{
+		v[i++] = margin + num++ * interval + now;
+		v[i++] = part;
+		now += part;
+	};
+
+	auto sum = total_length - margin - margin - static_cast<T>(count - 1) * interval;
+	auto tr = [sum](auto part) { return static_cast<T>(part * (std::is_floating_point_v<decltype(part)> ? sum : 1)); };
+
+	(iteration(tr(parts)), ...);
+	v[i++] = margin + num * interval + now;
+	v[i] = sum - now;
+
+	return v;
+}
