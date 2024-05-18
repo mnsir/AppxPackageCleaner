05a0f9571896b19af8445536537c8a2db2d289f3
mnsir <mnsir@ya.ru>
2024-05-18 08:03:49 +0300

    better ui

--- AppxPackageCleaner.cpp
+++ AppxPackageCleaner.cpp
@@ -16,6 +16,10 @@
 #include "json.hpp"
 #include "utils.h"
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #undef max
 
 
